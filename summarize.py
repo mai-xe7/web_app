8f5eeae6de59816fbf1c7c5e74a28f1f6d61014e
mai mahmoud <149944898+mai-xe7@users.noreply.github.com>
2024-04-23 00:25:31 +0200

    Delete summarize.py

--- summarize.py
+++
@@ -1,8 +0,0 @@
-from summarizer import Summarizer
-
-def summarize_text(text):
-    model = Summarizer()
-    summary = model(text)
-    return summary
-
-#you  use the bert-extractive-summarizer which works well with modern NLP approaches
